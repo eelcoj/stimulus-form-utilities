b961946c4d8c77040fd9db605689835576197843
eelco <eelco@needlesstosay.co>
2019-03-17 12:27:12 +0100

    added missing deps in index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import CharacterCount from "./src/character_count"
 import AutoResizeHeight from "./src/auto_resize_height"
+import { Clipboard } from "stimulus-form-utilities"
+import { CommandEnter } from "stimulus-form-utilities"
 
-export { CharacterCount, AutoResizeHeight }
+export { CharacterCount, AutoResizeHeight, Clipboard, CommandEnter }
