75e18990acb9b30880eccf6516e22e4e0723f4f1
eelco <git@eelcoj.com>
2020-01-18 17:27:48 +0100

    better packaging now including all utility helpers

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import CharacterCount from './src/character_count'
-// import AutoResizeHeight from './src/auto_resize_height'
-// import Clipboard from './src/clipboard'
-// import CommandEnter from './src/command_enter'
+import AutoResizeHeight from './src/auto_resize_height'
+import Clipboard from './src/clipboard'
+import CommandEnter from './src/command_enter'
 
-export { CharacterCount }
-// export { CharacterCount, AutoResizeHeight, Clipboard, CommandEnter }
+export { CharacterCount, AutoResizeHeight, Clipboard, CommandEnter }
